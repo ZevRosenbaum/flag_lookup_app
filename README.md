508ec128b3c8e7af2ffee480ca6f05f4bdcfbc35
Zev Rosenbaum <zevrosenbaum@Zevs-Air.fios-router.home>
2023-12-14 09:38:06 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Flag Lookup Webapp - WIP
+## <ins>Author</ins>: Zev Rosenbaum
+## <ins>Created</ins>: December 2023
+
+#### Program for users to upload a photo of a flag and find out what country that flag belongs to.
+
+Partially inspired by Sheldon Cooper's Fun With Flags!
+<br>
+![Image of the Fun With Flags skit from The Big Bang Theory tv show](https://media.glamour.com/photos/56957f54085ae0a85036f142/master/w_1600%2Cc_limit/entertainment-2014-11-mayim-jim-fun-with-flags-main.jpg)
---
+++ flag_comparison.py
@@ -0,0 +1,83 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import json
+import torch
+import open_clip
+import cv2
+from sentence_transformers import util
+from PIL import Image
+import os
+from os import listdir
+from flask import *
+
+app = Flask(__name__)
+
+def get_country_name(country_code):
+    f = open('./codes.json')
+    country_data = json.load(f)
+    f.close()
+
+    if country_code not in country_data:
+        return "Error: Unable to find country"
+    return country_data.get(country_code)
+
+def get_country_name_and_flag(flag_filename):
+    # plt.imshow(mpimg.imread(flag_filename))
+    # ax = plt.gca()
+    # ax.get_xaxis().set_visible(False)
+    # ax.get_yaxis().set_visible(False)
+
+    filename = flag_filename.split('/')[-1]
+    country_code = filename.split('.')[0]
+    return get_country_name(country_code), flag_filename
+
+# Following deep learning approach for comparing two images was copied from the following link: 
+# https://medium.com/scrapehero/exploring-image-similarity-approaches-in-python-b8ca0a3ed5a3
+
+# image processing model
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-16-plus-240', pretrained="laion400m_e32")
+model.to(device)
+
+def imageEncoder(img):
+    img1 = Image.fromarray(img).convert('RGB')
+    img1 = preprocess(img1).unsqueeze(0).to(device)
+    img1 = model.encode_image(img1)
+    return img1
+
+def generateScore(image1, image2):
+    test_img = cv2.imread(image1, cv2.IMREAD_UNCHANGED)
+    data_img = cv2.imread(image2, cv2.IMREAD_UNCHANGED)
+    img1 = imageEncoder(test_img)
+    img2 = imageEncoder(data_img)
+    cos_scores = util.pytorch_cos_sim(img1, img2)
+    score = round(float(cos_scores[0][0])*100, 2)
+    return score
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/find_country/', methods=['POST'])
+def find_country():
+    user_image = request.files['user_image']
+    user_image.save('./test_images/user_upload.png')
+    user_image = './test_images/user_upload.png'
+    error_message = "ERROR: CANNOT FIND A MATCH, PLEASE UPLOAD A CLEARER PHOTO"
+    if not user_image:
+        return render_template('index.html', country_name=error_message)
+    
+    MINIMUM_THRESHOLD = 70.00
+    folder_dir = "./flag_icons"
+    for images in os.listdir(folder_dir):
+        curr_threshold = 85.00
+        similarity = generateScore(f'{folder_dir}/{images}', user_image)
+        while curr_threshold >= MINIMUM_THRESHOLD:
+            if similarity >= curr_threshold:
+                country_name, flag = get_country_name_and_flag(f'{folder_dir}/{images}')
+                return render_template('index.html', country_name=country_name, flag=flag)
+            curr_threshold -= 5.00
+    return render_template('index.html', country_name=error_message, flag=None)
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8080, debug=True)
